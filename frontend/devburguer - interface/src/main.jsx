e88567ab0f682462013822701b2dfba68e0cdcec
ArielPetter <arielputzpetter@gmail.com>
2024-11-26 17:16:22 -0300

    Resolving merge conflicts

--- frontend/devburguer - interface/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-import { Login } from "./containers/login";
-
-ReactDom.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
-);
